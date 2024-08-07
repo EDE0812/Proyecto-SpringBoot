1c264060228583aec171dc3c01317af64b6731b0
EDE0812 <hilton.ede97@gmail.com>
2024-08-07 18:39:53 -0500

    Primera clase entity

---
+++ crudspring/src/main/java/com/crudspring/ede/crudspring/entities/Libros.java
@@ -0,0 +1,8 @@
+package com.crudspring.ede.crudspring.entities;
+
+public class Libros {
+    private String titulo;
+    private String autor;
+    private String editorial;
+    private String anio;
+}
